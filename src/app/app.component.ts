512d1ff73d7407f40306a9c5f21a581cc910e86e
Nivedyakv <nivedyakv2703@gmail.com>
2024-04-04 17:30:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AngularTestcase11';
+  title = 'Form';
+  form = { 
+          username: '', 
+          email: '', 
+          address:'',
+          dob:'',
+          phone:'',
+          password: '' };
+  onSubmit(): void {
+      console.log(this.form);
+  }
+  onReset(form: NgForm): void {
+      form.reset();
+  }
+  
 }
